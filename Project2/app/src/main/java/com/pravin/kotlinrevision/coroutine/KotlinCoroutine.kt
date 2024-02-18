fccf748a3372326429aae27be36ff40d2383d1b0
Pravin5551 <pravin.pawar@pagesuite.com>
2024-02-18 15:00:15 +0530

    java_dsa global scope used

--- Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
+++ Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
@@ -1,6 +1,9 @@
 package com.pravin.kotlinrevision.coroutine
 
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.concurrent.thread
 
 
@@ -8,29 +11,25 @@ fun main() {
     println("Hi from kotlin")
 
 
-    println("Start of ${Thread.currentThread().name} thread")
+    println("Starts on ${Thread.currentThread().name} thread")
 
 
-    thread {
-        println("Dummy work start on ${Thread.currentThread().name} thread")
-        Thread.sleep(1000)
-        println("Dummy work ends on ${Thread.currentThread().name} thread")
+    GlobalScope.launch {
+        println("Dummy work start on ${Thread.currentThread().name} Coroutine1")
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+        }
+        println("Dummy work ends on ${Thread.currentThread().name} Coroutine1")
     }
 
-
-
-    println("End of ${Thread.currentThread().name} thread")
-
-    thread {
-
-
-        println("Dummy work start on ${Thread.currentThread().name} thread")
-        Thread.sleep(1000)
-
-        println("Dummy work ends on ${Thread.currentThread().name} thread")
-
-
+    GlobalScope.launch {
+        println("Dummy work ends on ${Thread.currentThread().name} Coroutine2")
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+        }
+        println("Dummy work ends on ${Thread.currentThread().name} Coroutine2")
     }
 
-//    CoroutineScope(Cor)
+    println("Ends on ${Thread.currentThread().name} thread")
+
 }
