0ad5457af01255f4220797c72c3cc7d18186227f
pravin5551 <pravinvpawar5551@gmail.com>
2024-02-18 16:54:07 +0530

    used run blocking

--- Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
+++ Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
@@ -1,28 +1,45 @@
 package com.pravin.kotlinrevision.coroutine
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
 
 
-fun main() {
+@OptIn(DelicateCoroutinesApi::class)
+fun main() = runBlocking {
     println("Hi from kotlin")
 
 
     println("Starts on ${Thread.currentThread().name} thread")
 
 
+    /**
+     * GlobalScope.launch launches new  coroutine
+     */
+
+
+    /**
+     * GlobalScope.launch launches new  coroutine
+     */
     GlobalScope.launch {
         println("Dummy work start on ${Thread.currentThread().name} Coroutine1")
-        withContext(Dispatchers.IO) {
-            Thread.sleep(1000)
-        }
+//        withContext(Dispatchers.IO) {
+        //this will block the thread and not let the thread to to do anything
+//            Thread.sleep(2000)
+//        }
+
+        //Delay will not block thread it will just delay the work for some time, and let other task do
+        delay(1000)
         println("Dummy work ends on ${Thread.currentThread().name} Coroutine1")
     }
 
+    /*this will block the thread and not let the thread to to do anything */
 //    Thread.sleep(2000)
+    /**
+     * Same like Thread.sleep(2000) but in coroutine
+     */
+    runBlocking {
+        delay(2000)
+    }
     println("Ends on ${Thread.currentThread().name} thread")
 
 }
