bb908cb3cbe1511afc7999096e5c0b5c5282b77f
pravin5551 <pravinvpawar5551@gmail.com>
2024-02-19 23:28:20 +0530

    Example of  GlobalScope.launch

--- Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
+++ Project2/app/src/main/java/com/pravin/kotlinrevision/coroutine/KotlinCoroutine.kt
@@ -1,7 +1,6 @@
 package com.pravin.kotlinrevision.coroutine
 
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
 
 
 @OptIn(DelicateCoroutinesApi::class)
@@ -10,11 +9,6 @@ fun main() = runBlocking {
 
 
     println("Starts on ${Thread.currentThread().name} thread")
-
-
-    /**
-     * GlobalScope.launch launches new  coroutine
-     */
 
 
     /**
@@ -30,16 +24,52 @@ fun main() = runBlocking {
         //Delay will not block thread it will just delay the work for some time, and let other task do
         delay(1000)
         println("Dummy work ends on ${Thread.currentThread().name} Coroutine1")
+
+
     }
 
+    /**
+     * launch should always be called from any coroutine builder
+     */
+    val job: Job = launch {
+        println("inside launch")
+
+        var number = 8 * 9;
+        print("Number is $number")
+    }
+
+    //instead of stopping the thread just use job.join() and it will wait till the job is completed and then run the rest of the code
+    job.join()
+
+    // this cancels the job execution
+//    job.cancel()
     /*this will block the thread and not let the thread to to do anything */
 //    Thread.sleep(2000)
     /**
      * Same like Thread.sleep(2000) but in coroutine
+     *
      */
+
+
     runBlocking {
-        delay(2000)
+        delay(5000)
+    }
+
+    //or
+    GlobalScope.launch {
+        mySuspendFun(1, "lol")
     }
     println("Ends on ${Thread.currentThread().name} thread")
 
+
+//    launch {
+//        println("This is ${Thread.currentThread().name} thread")
+//    }
 }
+
+
+//Suspend should always be called from another suspend ort from coroutine builder
+suspend fun mySuspendFun(l: Int, p: String) {
+    delay(5000)
+}
+
