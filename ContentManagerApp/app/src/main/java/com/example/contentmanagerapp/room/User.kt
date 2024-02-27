32c86651f8e480a5de94638f5d243253d8dd0113
Pravin5551 <pravinvpawar5551@gmail.com>
2024-02-28 03:27:49 +0530

    User updatd

--- ContentManagerApp/app/src/main/java/com/example/contentmanagerapp/room/User.kt
+++ ContentManagerApp/app/src/main/java/com/example/contentmanagerapp/room/User.kt
@@ -2,10 +2,12 @@ package com.example.contentmanagerapp.room
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "UserData")
 data class User(
+    @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "user_id")
     val user_id: Int,
 
