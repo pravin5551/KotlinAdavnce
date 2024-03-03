1bbf979eeb61b2c73376d1f9975cbb5205767234
Pravin5551 <pravinvpawar5551@gmail.com>
2024-03-03 17:13:37 +0530

    first commit for DSA Project

---
+++ Project2/app/src/main/java/com/pravin/kotlinrevision/dsa/GSLabs.java
@@ -0,0 +1,21 @@
+package com.pravin.kotlinrevision.dsa;
+
+public class GSLabs {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countWays(n));
+    }
+
+    public static int countWays(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
